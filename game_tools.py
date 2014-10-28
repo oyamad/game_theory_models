5897995c94b5286ac8126283c9fc1d62d915d70f
kusanominal <ksneco@gmail.com>
2014-10-29 00:24:52 +0900

    Modified br_corr to deal with a pure action.

--- game_tools.py
+++ game_tools.py
@@ -12,82 +12,84 @@ import numpy as np
 
 
 class Player_2P(object):
-	"""
-	Class representing a player in a two-player normal form game.
+    """
+    Class representing a player in a two-player normal form game.
 
-	"""
-	def __init__(self, payoff_matrix, init_action=None):
-		self.payoff_matrix = np.asarray(payoff_matrix)
+    """
+    def __init__(self, payoff_matrix, init_action=None):
+        self.payoff_matrix = np.asarray(payoff_matrix)
 
-		if len(self.payoff_matrix.shape) != 2:
-			raise ValueError('payoff matrix must be 2-dimensional')
+        if len(self.payoff_matrix.shape) != 2: #ndim?
+            raise ValueError('payoff matrix must be 2-dimensional')
 
-		self.n, self.m = self.payoff_matrix.shape
+        self.n, self.m = self.payoff_matrix.shape
 
-		self.current_action = init_action
+        self.current_action = init_action
 
-	def best_response(self, opponent_action,
-					  tie_breaking=True, payoff_perturbations=None):
-		br_str = br_corr(opponent_action, self.payoff_matrix)
-		if tie_breaking:
-			return random_choice(br_str)
-		else:
-			return br_str
-	
-	def random_choice(self):
-		return random_choice(range(self.n))
+    def best_response(self, opponent_action,
+                      tie_breaking=True, payoff_perturbations=None):
+        br_actions = br_corr(opponent_action, self.payoff_matrix)
+        if tie_breaking:
+            return random_choice(br_actions)
+        else:
+            return br_actions
+    
+    def random_choice(self):
+        return random_choice(range(self.n))
 
 
 class NormalFormGame_2P(object):
-	"""
-	Class representing a two-player normal form game.
+    """
+    Class representing a two-player normal form game.
 
-	"""
-	player_indices = [0, 1]
+    """
+    player_indices = [0, 1]
+    def __init__(self, payoffs):
+        payoffs_ndarray = np.asarray(payoffs)
+        if payoffs_ndarray.ndim == 3:  # bimatrix game
+            self.bimatrix = payoffs_ndarray
+            self.matrices = [payoffs_ndarray[:, :, 0],
+                             payoffs_ndarray[:, :, 1].T]
+        elif payoffs_ndarray.ndim == 2:  # symmetric game
+            try:
+                self.bimatrix = np.dstack((payoffs_ndarray, payoffs_ndarray.T))
+            except:
+                raise ValueError('a symmetric game must be a square array')
+            self.matrices = [payoffs_ndarray, payoffs_ndarray]
+        else:
+            raise ValueError(
+                'the game must be represented by a bimatrix or a square matrix'
+            )
 
-	def __init__(self, payoffs):
-		payoffs_ndarray = np.asarray(payoffs)
-		if payoffs_ndarray.ndim == 3:  # bimatrix game
-			self.bimatrix = payoffs_ndarray
-			self.matrices = [payoffs_ndarray[:, :, 0],
-							 payoffs_ndarray[:, :, 1].T]
-		elif payoffs_ndarray.ndim == 2:  # symmetric game
-			try:
-				self.bimatrix = np.dstack((payoffs_ndarray, payoffs_ndarray.T))
-			except:
-				raise ValueError('a symmetric game must be a square array')
-			self.matrices = [payoffs_ndarray, payoffs_ndarray]
-		else:
-			raise ValueError(
-				'the game must be represented by a bimatrix or a square matrix'
-			)
-
-		self.players = [
-			Player_2P(self.matrices[i]) for i in self.player_indices
-		]
+        self.players = [
+            Player_2P(self.matrices[i]) for i in self.player_indices
+        ]
 
 
 def br_corr(mixed_action, payoff_matrix):
-	"""
-	Best response correspondence in pure actions.
+    """
+    Best response correspondence in pure actions.
 
-	"""
-	payoff_vec = np.dot(payoff_matrix, mixed_action)
-	return np.where(payoff_vec == payoff_vec.max())[0]
+    """
+    #np.asarray?
+    if isinstance(mixed_action, int):
+        mixed_action = pure2mixed(payoff_matrix.shape[0], mixed_action)
+    payoff_vec = np.dot(payoff_matrix, mixed_action)
+    return np.where(payoff_vec == payoff_vec.max())[0]
 
 
 def random_choice(actions):
-	if len(actions) == 1:
-		return actions[0]
-	else:
-		return np.random.choice(actions)
+    if len(actions) == 1:
+        return actions[0]
+    else:
+        return np.random.choice(actions)
 
 
 def pure2mixed(num_actions, action):
-	"""
-	Convert a pure action to the corresponding mixed action.
+    """
+    Convert a pure action to the corresponding mixed action.
 
-	"""
-	mixed_action = np.zeros(num_actions)
-	mixed_action[action] = 1
-	return mixed_action
+    """
+    mixed_action = np.zeros(num_actions)
+    mixed_action[action] = 1
+    return mixed_action
