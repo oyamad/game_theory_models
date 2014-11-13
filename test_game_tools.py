e3f48499118696523bede391e51ef4cfe9f990b2
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2014-11-13 16:59:30 +0900

    Some more tests added

--- test_game_tools.py
+++ test_game_tools.py
@@ -50,8 +50,32 @@ class TestPlayer_1opponent:
         ok_(self.player.is_best_response([1/2, 1/2], [2/3, 1/3]))
 
 
-class TestNormalFormGame_2p:
-    """Test the methods of NormalFormGame with two players"""
+class TestPlayer_2opponents:
+    """Test the methods of Player with two opponent players"""
+
+    def setUp(self):
+        """Setup a Player instance"""
+        payoffs_2opponents = [[[3, 6],
+                               [4, 2]],
+                              [[1, 0],
+                               [5, 7]]]
+        self.player = Player(payoffs_2opponents)
+
+    def test_best_response_against_pure(self):
+        """best_response against a pure action"""
+        eq_(self.player.best_response((1, 1)), 1)
+
+    def test_best_response_list_when_tie(self):
+        """best_response with tie_breaking=False"""
+        assert_array_equal(
+            sorted(self.player.best_response(([3/7, 4/7], [1/2, 1/2]),
+                                             tie_breaking=False)),
+            sorted([0, 1])
+        )
+
+
+class TestNormalFormGame_Sym2p:
+    """Test the methods of NormalFormGame with symmetric two players"""
 
     def setUp(self):
         """Setup a NormalFormGame instance"""
@@ -67,27 +91,22 @@ class TestNormalFormGame_2p:
         ok_(self.g.is_nash(([2/3, 1/3], [2/3, 1/3])))
 
 
-class TestBRCorrespondence_2opponents:
-    """Test br_correspondence with two opponents"""
+class TestNormalFormGame_Asym2p:
+    """Test the methods of NormalFormGame with asymmetric two players"""
 
     def setUp(self):
-        """Set up a payoff function with two opponents"""
-        self.payoffs_2opponents = [[[3, 6],
-                                    [4, 2]],
-                                   [[1, 0],
-                                    [5, 7]]]
+        """Setup a NormalFormGame instance"""
+        matching_pennies_matrix = [[( 1, -1), (-1,  1)],
+                                   [(-1,  1), ( 1, -1)]]
+        self.g = NormalFormGame(matching_pennies_matrix)
 
-    def test_br_correspondence_against_pure(self):
-        """br_correspondence against a profile of pure actions"""
-        eq_(br_correspondence([0, 0], self.payoffs_2opponents), 0)
+    def test_is_nash_pure(self):
+        """is_nash with pure actions"""
+        ok_(not self.g.is_nash((0, 0)))
 
-    def test_br_correspondence_against_mixed(self):
-        """br_correspondence against a profile of mixed actions"""
-        assert_array_equal(
-            sorted(br_correspondence([[3/7, 4/7], [1/2, 1/2]],
-                                     self.payoffs_2opponents)),
-            sorted([0, 1])
-        )
+    def test_is_nash_mixed(self):
+        """is_nash with mixed actions"""
+        ok_(self.g.is_nash(([1/2, 1/2], [1/2, 1/2])))
 
 
 if __name__ == '__main__':
