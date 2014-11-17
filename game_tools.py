c9e915e424f7741bee8c077f38d8d0eb073ebb4f
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2014-11-17 11:46:44 +0900

    Minor modification

--- game_tools.py
+++ game_tools.py
@@ -170,7 +170,10 @@ class Player(object):
             np.where(np.isclose(payoff_vector, payoff_vector.max()))[0]
 
         if tie_breaking:
-            return random_choice(best_responses)
+            if len(best_responses) == 1:
+                return best_responses[0]
+            else:
+                return random_choice(best_responses)
         else:
             return best_responses
 
