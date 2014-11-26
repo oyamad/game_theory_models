d58f38eab3cf1af3fcc57c65321d1f5c06cd44b0
kusanominal <ksneco@gmail.com>
2014-11-26 23:28:41 +0900

    Rearranged to comply to PEP8.

--- test_localint.py
+++ test_localint.py
@@ -14,6 +14,7 @@ from nose.tools import eq_, ok_, raises
 
 from localint import LocalInteraction
 
+
 class TestLocalInteraction:
     '''Test the methods of LocalInteraction'''
 
@@ -33,7 +34,7 @@ class TestLocalInteraction:
 
     def test_set_init_actions_when_init_actions_None(self):
         self.li.set_init_actions()
-        ok_(all(action in list(range(2)) \
+        ok_(all(action in list(range(2))
             for action in self.li.current_actions))
 
     def test_play_when_player_ind_None(self):
@@ -45,7 +46,7 @@ class TestLocalInteraction:
         assert_array_equal(self.li.current_actions, [1, 1, 0])
 
     def test_play_when_player_ind_list(self):
-        self.li.play(player_ind=[1,2])
+        self.li.play(player_ind=[1, 2])
         assert_array_equal(self.li.current_actions, [1, 1, 1])
 
     def test_simulate_with_simultaneous_revision(self):
@@ -59,25 +60,26 @@ class TestLocalInteraction:
     def test_simulate_with_sequential_revison(self):
         np.random.seed(5)
         assert_array_equal(
-            self.li.simulate(ts_length=3, init_actions=[1, 0, 0], \
-                revision='sequential'),
+            self.li.simulate(ts_length=3, init_actions=[1, 0, 0],
+                             revision='sequential'),
             [[1, 0, 0],
              [1, 0, 1],
              [1, 1, 1]]
             )
 
+
 # Invalid inputs #
 
 @raises(ValueError)
 def test_localint_invalid_input_nonsquare_adj_matrix():
-    li = LocalInteraction(payoff_matrix=np.zeros((2,2)), \
-                          adj_matrix=np.zeros((2,3)))
+    li = LocalInteraction(payoff_matrix=np.zeros((2, 2)),
+                          adj_matrix=np.zeros((2, 3)))
 
 
 @raises(ValueError)
 def test_localint_invalid_input_nonsquare_payoff_matrix():
-    li = LocalInteraction(payoff_matrix=np.zeros((2,3)), \
-                          adj_matrix=np.zeros((2,2)))
+    li = LocalInteraction(payoff_matrix=np.zeros((2, 3)),
+                          adj_matrix=np.zeros((2, 2)))
 
 if __name__ == '__main__':
     import sys
