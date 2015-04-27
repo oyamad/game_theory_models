c4b272e39e3d6001c9044ea576d17d4e98b07153
kusanominal <ksneco@gmail.com>
2015-04-28 02:53:32 +0900

    Test for set_choice_probs added, test_simulate modified.

--- test_logitdyn.py
+++ test_logitdyn.py
@@ -8,7 +8,8 @@ Tests for logitdyn.py
 from __future__ import division
 
 import numpy as np
-from numpy.testing import assert_array_equal, assert_almost_equal
+from numpy.testing import assert_array_equal, assert_almost_equal, \
+    assert_array_almost_equal_nulp
 from nose.tools import eq_, ok_, raises
 
 from logitdyn import LogitDynamics
@@ -28,7 +29,6 @@ class TestLogitDynamics:
 
     def test_set_init_actions_with_given_init_actions(self):
         init_actions = (0, 1)
-
         self.ld.set_init_actions(init_actions)
         assert_array_equal(self.ld.current_actions, init_actions)
 
@@ -40,61 +40,44 @@ class TestLogitDynamics:
             action in list(range(2)) for action in self.ld.current_actions)
             )
 
-    def test_play_seed(self):
-        init_actions = (0, 1)
-        self.ld.set_init_actions(init_actions)
-        np.random.seed(0)
-        self.ld.play(0)
-        actions1 = np.array(self.ld.current_actions)
-        self.ld.set_init_actions(init_actions)
-        np.random.seed(7)
-        self.ld.play(0)
-        actions2 = np.array(self.ld.current_actions)
-        action_profiles = np.vstack((actions1, actions2))
-        assert_array_equal(
-            action_profiles,
-            [[1, 1],
-             [0, 1]]
-            )
-
-    def test_play_LLN(self):
-        init_actions = (0, 1)
-        count = 0
-        for i in range(100000):
-            self.ld.set_init_actions(init_actions)
-            self.ld.play(0)
-            if self.ld.current_actions[0] == 1:
-                count += 1
-        assert_almost_equal(count/100000, 0.881, decimal=2)
+    def test_set_choice_probs_with_asymmetric_payoff_matrix(self):
+        asym_payoffs = [[(4, 4), (1, 1), (0, 3)], [(3, 0), (1, 1), (2, 2)]]
+        g_asym = NormalFormGame(asym_payoffs)
+        self.ld = LogitDynamics(g_asym)
+        assert_array_almost_equal_nulp(
+            self.ld.players[0].logit_choice_cdfs,
+            [[1., 1.36787944],
+             [1., 2.],
+             [0.13533528, 1.13533528]],
+            nulp=1000000000
+        )
 
     def test_simulate_seed(self):
-        np.random.seed(7)
-        seq = self.ld.simulate(ts_length=10)
+        np.random.seed(0)
+        seq = self.ld.simulate(ts_length=10, init_actions=(0, 0))
         assert_array_equal(
             seq,
-            [[1, 0],
-             [1, 1],
-             [0, 1],
-             [0, 0],
+            [[0, 0],
              [0, 0],
              [0, 0],
              [0, 1],
              [1, 1],
-             [1, 0],
-             [0, 0]]
+             [1, 1],
+             [1, 1],
+             [1, 1],
+             [1, 1],
+             [1, 1]]
         )
 
     def test_simulate_LLN(self):
-        counts = np.zeros(1000)
-        for i in range(1000):
-            seq = self.ld.simulate(ts_length=100)
-            count = 0
-            for j in range(100):
-                if all(seq[j, :] == [1, 1]):
-                    count += 1
-            counts[i] = count
-        m = counts.mean() / 100
-        assert_almost_equal(m, 0.6, decimal=1)
+        n = 1000
+        seq = self.ld.replicate(T=100, num_reps=n)
+        count = 0
+        for i in range(n):
+            if all(seq[i, :] == [1, 1]):
+                count += 1
+        ratio = count / n
+        assert_almost_equal(ratio, 0.61029569, decimal=1)
 
 
 if __name__ == '__main__':
