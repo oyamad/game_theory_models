fa8de77a398f3d2aae89896e5f3cdb8bc9be6354
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2015-04-30 23:28:04 +0900

    Revised
    - value of `beta` changed from 1.0 to 4.0 in TestLogitDynamics
    - `test_set_choice_probs_with_asymmetric_payoff_matrix` made an independent function
      probabilities computed within the function

--- test_logitdyn.py
+++ test_logitdyn.py
@@ -8,8 +8,7 @@ Tests for logitdyn.py
 from __future__ import division
 
 import numpy as np
-from numpy.testing import assert_array_equal, assert_almost_equal, \
-    assert_array_almost_equal_nulp
+from numpy.testing import assert_array_equal, assert_array_almost_equal_nulp
 from nose.tools import eq_, ok_, raises
 
 from logitdyn import LogitDynamics
@@ -24,8 +23,9 @@ class TestLogitDynamics:
         # symmetric 2x2 coordination game
         payoff_matrix = [[4, 0],
                          [3, 2]]
+        beta = 4.0
         g = NormalFormGame(payoff_matrix)
-        self.ld = LogitDynamics(g)
+        self.ld = LogitDynamics(g, beta=beta)
 
     def test_set_init_actions_with_given_init_actions(self):
         init_actions = (0, 1)
@@ -40,20 +40,8 @@ class TestLogitDynamics:
             action in list(range(2)) for action in self.ld.current_actions)
             )
 
-    def test_set_choice_probs_with_asymmetric_payoff_matrix(self):
-        asym_payoffs = [[(4, 4), (1, 1), (0, 3)], [(3, 0), (1, 1), (2, 2)]]
-        g_asym = NormalFormGame(asym_payoffs)
-        self.ld = LogitDynamics(g_asym)
-        assert_array_almost_equal_nulp(
-            self.ld.players[0].logit_choice_cdfs,
-            [[1., 1.36787944],
-             [1., 2.],
-             [0.13533528, 1.13533528]],
-            nulp=1000000000
-        )
-
     def test_simulate_seed(self):
-        np.random.seed(0)
+        np.random.seed(291)
         seq = self.ld.simulate(ts_length=10, init_actions=(0, 0))
         assert_array_equal(
             seq,
@@ -69,15 +57,36 @@ class TestLogitDynamics:
              [1, 1]]
         )
 
-    def test_simulate_LLN(self):
-        n = 1000
-        seq = self.ld.replicate(T=100, num_reps=n)
+    def test_simulate_lln(self):
+        n = 100
+        T = 1000
+        seq = self.ld.replicate(T=T, num_reps=n)
         count = 0
         for i in range(n):
             if all(seq[i, :] == [1, 1]):
                 count += 1
-        ratio = count / n
-        assert_almost_equal(ratio, 0.61029569, decimal=1)
+        frequency = count / n
+
+        # 0.981367209 = prob that the stationary distribution assigns to [1, 1]
+        ok_(np.abs(frequency-0.981367209) < 0.05)
+
+
+def test_set_choice_probs_with_asymmetric_payoff_matrix():
+    bimatrix = np.array([[(4, 4), (1, 1), (0, 3)],
+                             [(3, 0), (1, 1), (2, 2)]])
+    beta = 1.0
+    g = NormalFormGame(bimatrix)
+    ld = LogitDynamics(g, beta=beta)
+
+    # (Normalized) CDFs of logit choice
+    cdfs = np.ones((bimatrix.shape[1], bimatrix.shape[0]))
+    cdfs[:, 0] = 1 / (1 + np.exp(beta*(bimatrix[1, :, 0]-bimatrix[0, :, 0])))
+
+    # self.ld.players[0].logit_choice_cdfs: unnormalized
+    cdfs_computed = ld.players[0].logit_choice_cdfs
+    cdfs_computed = cdfs_computed / cdfs_computed[..., [-1]]  # Normalized
+
+    assert_array_almost_equal_nulp(cdfs_computed, cdfs)
 
 
 if __name__ == '__main__':
